220bffd18118eda632f661414a16e6e4b946e749
chenwanhai <chenwanhai@sina.com>
2017-07-31 14:07:00 +0800

    ddddddd

--- maven-test/src/main/java/maven_test/maven_test/App.java
+++ maven-test/src/main/java/maven_test/maven_test/App.java
@@ -4,11 +4,15 @@ package maven_test.maven_test;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
         // testtest
+
+        // dev直接提交到master
+
+        // zaizai
+
+        // dddd
     }
 }
